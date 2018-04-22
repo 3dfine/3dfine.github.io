929345724a0f42a7d9d921b443e3b6e86d893240
3dfine <3dfine@mail.ru>
2018-04-22 13:18:48 +0300

    cg

--- js/finalOutput.js
+++ js/finalOutput.js
@@ -85,7 +85,7 @@ let animate = function () {
   requestAnimationFrame( animate );
   tick += 0.075;
   //здесь выполняется после загрузки 3д моделей
-  if( globalLoad > 25 ) {
+  if( globalLoad > 30 ) {
     animateCamera1( CameraKeyTrck );
     animHolodUst1(tick);
     controls.update();
